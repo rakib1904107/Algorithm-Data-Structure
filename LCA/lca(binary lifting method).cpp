617b9ce9fe41e7b60a9078ce717ba57c5b05e902
rakibalhasan <rakibalhasan1904@gmail.com>
2024-09-26 21:43:22 +0600

    lca

--- LCA/lca(binary lifting method).cpp
+++ LCA/lca(binary lifting method).cpp
@@ -1,68 +1,59 @@
 #include<bits/stdc++.h>
-#define mx 100002
 #define FIO() ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-int L[mx],T[mx];///level and parent array
-int P[mx][30];///sparse table
-int n,m;
-vector<int>adj[mx];
-void dfs(int par,int node,int lev){
-    T[node]=par;
-    L[node]=lev;
-    for(int child:adj[node]){
-        if(child!=par)
-            dfs(node,child,lev+1);
-    }
-}
-void lca_init(){///form sparse table
-    memset(P,-1,sizeof(P));
-    for(int i=1;i<=n;i++)
-        P[i][0]=T[i];
 
-    for(int j=1;(1<<j)<n;j++){
-        for(int i=1;i<=n;i++){
-            if(P[i][j-1]!=-1)
-                P[i][j]=P[P[i][j-1]][j-1];
+const int MX=100005;
+const int LOG=18;
+vector<int> adj[MX];
+int depth[MX];
+int up[MX][LOG];
+
+void dfs(int node,int parent) {
+    up[node][0]=parent;
+    for (int i=1;i<LOG;++i) {
+        up[node][i]=up[up[node][i-1]][i-1];
+    }
+    for (int neighbor : adj[node]) {
+        if (neighbor!= parent) {
+            depth[neighbor]=depth[node]+1;
+            dfs(neighbor, node);
         }
     }
 }
-int lca_query(int p,int q){///find LCA
-    if(L[p]<L[q])swap(p,q);
 
-    int log=1;
-    while(1) {///or log=log2(L[p]) but it may occure precission error
-        int next=log+1;
-        if((1<<next)>L[p])break;
-        log++;
+int lca(int u, int v) {
+    if (depth[u] < depth[v]) swap(u, v);
+    for (int i = LOG - 1; i >= 0; --i) {
+        if (depth[u] - (1 << i) >= depth[v]) {
+            u = up[u][i];
+        }
     }
-
-    for(int i=log;i>=0;i--){
-        if(L[p]-(1<<i)>=L[q])
-            p=P[p][i];
-    } 
-
-    if(p==q)return p;
-
-    for(int i=log;i>=0;i--){
-        if(P[p][i]!=-1&&P[p][i]!=P[q][i])
-            p=P[p][i],q=P[q][i];
+    if (u == v) return u;
+    for (int i = LOG - 1; i >= 0; --i) {
+        if (up[u][i] != up[v][i]) {
+            u = up[u][i];
+            v = up[v][i];
+        }
     }
-
-    return T[p];
+    return up[u][0];
 }
 int main()
 {
     FIO();
-
-    cin>>n>>m;
+    int n;
+    cin>>n;
     int u,v;
-    for(int i=0;i<n;i++){
+    for(int i=1;i<n;i++){
         cin>>u>>v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    dfs(-1,1,0);
-    lca_init();
-    cout<<lca_query(6,3)<<endl;
+    dfs(1,-1);
+    int q;
+    cin>>q;
+    while(q--){
+        cin>>u>>v;
+        cout<<lca(u,v)<<endl;
+    }
 }
